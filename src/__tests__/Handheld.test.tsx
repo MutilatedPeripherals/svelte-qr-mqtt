9283a57f6c99efbad7a1a19593e59690d0f65034
Martin Põhjakivi <martin.pohjakivi@gmail.com>
2023-08-01 20:12:40 +0300

    fix test

--- src/__tests__/Handheld.test.tsx
+++ src/__tests__/Handheld.test.tsx
@@ -2,6 +2,12 @@ import {render} from "@testing-library/react";
 import React from "react";
 import HandheldContainer from "../components/HandheldContainer";
 
+vi.mock("react-router-dom", () => ({
+    useLocation: () => ({
+        pathname: "localhost:3000/example/path"
+    })
+}))
+
 test('Renders Handheld page correctly', async () => {
     render(<HandheldContainer/>);
 });
