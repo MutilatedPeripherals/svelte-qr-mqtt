d8f0cd79729442f6c22c09de0ad2d7d7c0b682f3
Martin Põhjakivi <martin.pohjakivi@gmail.com>
2023-08-02 15:54:09 +0300

    replace svelte-router-spa with svelte-spa-router

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,9 @@
 import Home from "./components/Home.svelte";
 import Handheld from "./components/Handheld.svelte";
 
-const routes = [
-    {
-        name: '/react-qr-mqtt',
-        component: Home
-    },
-    {
-        name: '/react-qr-mqtt/handheld',
-        component: Handheld
-    }
-]
+const routes = {
+    '/': Home,
+    '/handheld': Handheld,
+}
 
 export {routes}
