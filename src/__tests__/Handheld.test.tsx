cf0327e54f5f0549c4134e2e91d53be5d745e9e9
Martin Põhjakivi <martin.pohjakivi@gmail.com>
2023-08-03 11:46:26 +0300

    Delete last react component & remove react deps (except react-uuid)

--- src/__tests__/Handheld.test.tsx
+++
@@ -1,8 +0,0 @@
-import {render} from "@testing-library/react";
-import React from "react";
-import HandheldContainer from "../components/HandheldContainer";
-
-test('Renders Handheld page correctly', async () => {
-    render(<HandheldContainer/>);
-});
-
--- vitest.config.js
+++ vitest.config.js
@@ -1,15 +1,13 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config'
 import {svelte} from "@sveltejs/vite-plugin-svelte";
 
 // TODO: check if this is still necessary once fully migrated to Svelte
 export default defineConfig({
     plugins: [
-        react(),
         svelte({hot: !process.env.VITEST})
     ],
     test: {
-        include: ['**/*.test.tsx'],
+        include: ['**/*.test.ts'],
         globals: true,
         environment: 'jsdom'
     },
