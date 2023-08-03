f242b2faf724b5da6bc26237fd0cf9a8e780d37f
Martin Põhjakivi <martin.pohjakivi@gmail.com>
2023-08-03 18:57:51 +0300

    add 'resolve alias' vite configuration for tests as well

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config'
 import {svelte} from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 // TODO: check if this is still necessary once fully migrated to Svelte
 export default defineConfig({
+    resolve: {
+        alias: {
+            src: path.resolve(__dirname, './src')
+        }
+    },
     plugins: [
         svelte({hot: !process.env.VITEST})
     ],
