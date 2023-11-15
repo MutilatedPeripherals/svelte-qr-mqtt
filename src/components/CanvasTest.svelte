67492557b32283151b52528be8938ca0a570d690
linomp <lm2p@hotmail.es>
2023-11-15 22:07:22 +0200

    create separate canvas component

---
+++ src/components/CanvasTest.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import {onMount} from "svelte";
+    import * as PIXI from "pixi.js";
+
+    onMount(async () => {
+        var app = new PIXI.Application();
+        var renderer = await PIXI.autoDetectRenderer(
+            window.innerWidth,
+            window.innerHeight
+        );
+        console.log("renderer", renderer);
+
+        await app.init({
+            width: window.innerWidth,
+            height: window.innerHeight,
+            backgroundColor: "pink",
+        });
+        console.log("app", app.canvas);
+
+        document.getElementById("canvas-container")?.appendChild(app.canvas);
+
+        let helloText = new PIXI.Text("Hello World", {
+            fontFamily: "Arial",
+            fontSize: 24,
+            fill: 0x000000,
+            align: "center",
+        });
+        helloText.x = app.screen.width / 2;
+        helloText.y = app.screen.height / 2;
+        helloText.anchor.set(0.5); // Center anchor point
+        app.stage.addChild(helloText);
+    });
+
+</script>
+
+<div id="canvas-container"></div>
--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,11 @@ import wrap from "svelte-spa-router/wrap";
 
 const routes = {
     '/': wrap({
-        asyncComponent: () => import('./components/Home.svelte')
+        asyncComponent: () => import('./components/CanvasTest.svelte')
     }),
+    // '/': wrap({
+    //     asyncComponent: () => import('./components/Home.svelte')
+    // }),
     '/handheld': wrap({
         asyncComponent: () => import('./components/Handheld.svelte')
     }),
